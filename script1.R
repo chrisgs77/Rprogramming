f77544adeef3a86ff07af43bd3c849047ef1b53d
chrisgs77 <scott.christopher@mayo.edu>
2020-04-01 11:41:54 -0500

    updated hw and code

--- script1.R
+++ script1.R
@@ -2,3 +2,10 @@ myfunction <- function () {
   x<-rnorm(100)
   mean(x)
 }
+hw<-read.csv("hw1_data.csv")
+names(hw)
+hw[1:2,]
+hw2<-hw[hw$Ozone>31,]
+hw2<-hw[hw$Ozone>31 & hw$Temp>90,]
+hw3<-hw[hw$Month==6,]
+hw4<-hw[hw$Month==5,]
